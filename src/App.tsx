c41bc2bd0506e88fe6f6a70906dee94a2c7ae700
Kartik Saini <kackysaini8@gmail.com>
2024-08-05 00:53:59 +0530

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Simulation from "./components/Simulation";
 import Game from "./components/Game";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/simulation" element={<Simulation />} />
         <Route path="/game" element={<Game />} />
       </Routes>
     </BrowserRouter>
